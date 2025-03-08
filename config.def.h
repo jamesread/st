725692a11aabbd6c4c18d525d885b20017d928e1
jamesread <contact@jread.com>
2025-03-08 01:52:22 +0000

    Add nerdfont :-)

--- config.def.h
+++ config.def.h
@@ -5,7 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+//static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Cascadia Code NF:style=Regular:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
